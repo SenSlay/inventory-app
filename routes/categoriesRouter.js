b7fa12a521a8f652db2aa2273bbb166e4021ee36
Irvan Castro <castroirvan@gmail.com>
2025-04-02 20:48:17 +0800

    set up category routes

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,7 +1,10 @@
 const { Router } = require('express');
+const { getCategories, getCategory } = require('../controllers/categoriesController');
+const { getCategoryById } = require('../db/queries');
 
 const categoriesRouter = Router();
 
-
+categoriesRouter.get('/', getCategories);
+categoriesRouter.get('/:id', getCategory);
 
 module.exports = categoriesRouter;
