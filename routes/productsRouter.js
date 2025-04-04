2257caa13faab7a4042de6c733e07d67d135ee27
Irvan Castro <castroirvan@gmail.com>
2025-04-04 23:32:24 +0800

    add routes for get and post

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
-const { getProducts } = require('../controllers/productsController');
+const { getProducts, getProductDetails, getProductForm, addNewProduct } = require('../controllers/productsController');
 
 const productsRouter = Router();
 
 productsRouter.get('/', getProducts)
+productsRouter.get('/new', getProductForm)
+productsRouter.get('/:id', getProductDetails);
+productsRouter.get('/:id/edit', getProductForm);
+
+productsRouter.post('/new', addNewProduct);
 
 module.exports = productsRouter;
