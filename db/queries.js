22cfd59a57f1699af18710b0fc5d59c923c1ca41
Irvan Castro <castroirvan@gmail.com>
2025-04-04 14:14:47 +0800

    add queries for select, insert and updating category

--- db/queries.js
+++ db/queries.js
@@ -25,10 +25,33 @@ async function getSubCategoriesById(id) {
   return rows;
 }
 
+async function getProductsByCategoryId(id) {
+  const { rows } = await pool.query("SELECT * FROM inventory WHERE category_id = ($1)", [id]);
+  return rows;
+}
+
+async function insertCategory(category, description, parentId) {
+  const result = await pool.query(
+    "INSERT INTO categories (category, description, parent_category_id) VALUES ($1, $2, $3) RETURNING *",
+    [category, description, parentId]
+  );
+
+  return result.rows[0];
+}
+
+async function updateCategory(id, category, description) {
+  const result = await pool.query("UPDATE categories SET category = $1, description = $2 WHERE id = $3 RETURNING *", [category, description, id]);
+
+  return result.rows[0];
+}
+
 module.exports = {
   getAllTopCategories,
   getAllSubCategories,
   getallInventory,
   getCategoryById,
-  getSubCategoriesById
+  getSubCategoriesById,
+  getProductsByCategoryId,
+  insertCategory,
+  updateCategory
 }
