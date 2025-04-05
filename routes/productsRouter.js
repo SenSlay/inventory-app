5f20a587ea519c7a69479f56e3cc88a2c1944a88
Irvan Castro <castroirvan@gmail.com>
2025-04-05 14:09:08 +0800

    create routes for post of create/edit

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,13 +1,27 @@
 const { Router } = require('express');
-const { getProducts, getProductDetails, getProductForm, addNewProduct } = require('../controllers/productsController');
+const multer = require('multer');
+const path = require('path');
+const { getProducts, getProductDetails, getProductForm, addNewProduct, editProduct } = require('../controllers/productsController');
 
 const productsRouter = Router();
+
+// Configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'public/uploads/'),
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage });
 
 productsRouter.get('/', getProducts)
 productsRouter.get('/new', getProductForm)
 productsRouter.get('/:id', getProductDetails);
 productsRouter.get('/:id/edit', getProductForm);
 
-productsRouter.post('/new', addNewProduct);
+productsRouter.post('/new', upload.single('image'), addNewProduct);
+productsRouter.post('/:id/edit', upload.single('image'), editProduct);
 
 module.exports = productsRouter;
