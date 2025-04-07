45354492db2d7a65b17249e3562210e1777b645d
Irvan Castro <castroirvan@gmail.com>
2025-04-07 13:34:11 +0800

    add ssl setting

--- db/populatedb.js
+++ db/populatedb.js
@@ -78,6 +78,9 @@ async function main() {
   console.log("seeding...");
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
 
   try {
