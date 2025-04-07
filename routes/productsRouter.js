6a8cb992ea70e065a441ad547e632ceb5f324e9b
Irvan Castro <castroirvan@gmail.com>
2025-04-07 18:39:11 +0800

    create cloudinary config and use it for image upload

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,20 +1,10 @@
 const { Router } = require('express');
-const multer = require('multer');
-const path = require('path');
 const { getProducts, getProductDetails, getProductForm, addNewProduct, editProduct, deleteProduct } = require('../controllers/productsController');
 
+// Import Cloudinary and upload settings
+const { upload } = require('../cloudinaryConfig');
+
 const productsRouter = Router();
-
-// Configure storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'public/uploads/'),
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  }
-});
-
-const upload = multer({ storage });
 
 productsRouter.get('/', getProducts)
 productsRouter.get('/new', getProductForm)
